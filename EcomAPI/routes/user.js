d666a1cff92d1beb92f181886d331df5f40dc33d
Mengran Zhu <zzmmrr0504@gmail.com>
2016-05-23 12:41:11 -0400

    add cart

--- EcomAPI/routes/user.js
+++ EcomAPI/routes/user.js
@@ -1,5 +1,7 @@
 var router = require('express').Router();
 var User = require('../models/user');
+var Cart = require('../models/cart');
+var async = require('async');
 var passport = require('passport');
 var passportConf = require('../config/passport');
 
@@ -35,29 +37,54 @@ router.get('/signup', function(req, res, next) {
 
 
 router.post('/signup', function(req, res, next){
-		var user = new User();
-		user.profile.name = req.body.name;
-		user.password = req.body.password;
-		user.email = req.body.email;
-		user.profile.picture = user.gravatar();
 
-		User.findOne({email: req.body.email}, function(err, existingUser) {
+		async.waterfall([
+			function(callback) {
+
+			var user = new User();
+
+
+			user.profile.name = req.body.name;
+			user.password = req.body.password;
+			user.email = req.body.email;
+			user.profile.picture = user.gravatar();
+
+			User.findOne({email: req.body.email}, function(err, existingUser) {
 			 if (existingUser) {
 			 		req.flash('errors', 'Account with that email address already exists!');
 			 		return res.redirect('/signup');
 			 } else {
 			 	user.save(function(err, user) {
 			 		if (err) return next(err);
+			 		callback(null, user);
 
-			 			req.logIn(user, function(err) {
-			 				if (err) return next(err);
-			 				res.redirect('/profile');
-			 			});
 			 	});
 			 }
 		});
 
+
+
+		},
+
+			function(user) {
+				var cart = new Cart();
+				cart.owner = user._id;
+				cart.save(function(err) {
+				if (err) return next(err);
+					req.logIn(user, function(err) {
+			 		if (err) return next(err);
+			 		res.redirect('/profile');
+			 		});
+
+
+				});
+		}
+
+	])
+
 });
+
+
 
 // logout route
 router.get('/logout',function(req, res, next) {
